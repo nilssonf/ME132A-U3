b837f48b743264b07a7dd5bdd2406ff0bb75abe9
Fanny Nilsson <fannyy.nilsson@gmail.com>
2022-03-31 16:57:08 +0200

    Finishing touches

--- courses.js
+++ courses.js
@@ -1,154 +1,180 @@
 'use strict'
 
-let inputCourse = document.getElementById('courses-search');
+let inputCourse = document.getElementById('courses-search')
+
+// för att sökfältet ska vara tomt vid reload
+window.onload = function () {
+  inputCourse.value = ''
+}
 
 // hitta courserna och sortera dessa i alfabetisk ordning
 function findCourse () {
-  let course = DATABASE.courses
-    .filter((course) => course.title.toLowerCase().includes(inputCourse.value));
+  let course = DATABASE.courses.filter(course =>
+    course.title.toLowerCase().includes(inputCourse.value)
+  )
 
-    course.sort(function (a, b) {
-        if (a.title > b.title) {
-            return 1;
-        } 
-        if (a.title < b.title) {
-            return -1;
-        }
-        return 0; 
-    });
-    return course; 
+  course.sort(function (a, b) {
+    if (a.title > b.title) {
+      return 1
+    }
+    if (a.title < b.title) {
+      return -1
+    }
+    return 0
+  })
+  return course
 }
 
 // eventlyssnare för sökfältet
-inputCourse.addEventListener("keyup", function(){
-    let foundCourse = findCourse ();
-    document.getElementById("results").innerHTML = "";
-    createHTML(foundCourse);
+inputCourse.addEventListener('keyup', function () {
+  let foundCourse = findCourse()
+  document.getElementById('results').innerHTML = ''
+  createHTML(foundCourse)
 
-    if (inputCourse.value == ""){
-        document.getElementById("results").innerHTML = "";
-    }
-});
+  if (inputCourse.value == '') {
+    document.getElementById('results').innerHTML = ''
+  }
+})
 
 // visa kursen med dess rätta innehåll i resultat-diven
 function renderCourse (course) {
-    let results = document.getElementById("results");
-    let div = document.createElement("div");
-    let courseTitle = document.createElement("h2");
-    div.classList.add("studentContainer");
+  let results = document.getElementById('results')
+  let div = document.createElement('div')
+  let courseTitle = document.createElement('h2')
+  div.classList.add('studentContainer')
 
-    courseTitle.innerText = course.title + " (" + course.totalCredits + " credits)";
-    results.appendChild(courseTitle);
-    results.appendChild(div);
+  courseTitle.innerText =
+    course.title + ' (' + course.totalCredits + ' credits)'
+  results.appendChild(courseTitle)
+  results.appendChild(div)
 
-    let foundStudents = getStudentsById(course);
-    let theResponsibleTeacher = getResponsibleTeacher(course);
-    let allTeachers = getAllTeachers(course);
-    let studentCourses = [];
-    
-    for (let student of foundStudents){
-        for (let studentCourse of student.courses){
-            if (studentCourse.courseId == course.courseId){
-                studentCourses.push(studentCourse);
-            }
-        }
+  let foundStudents = getStudentsById(course)
+  let theResponsibleTeacher = getResponsibleTeacher(course)
+  let allTeachers = getAllTeachers(course)
+  let studentCourses = []
+
+  for (let student of foundStudents) {
+    for (let studentCourse of student.courses) {
+      if (studentCourse.courseId == course.courseId) {
+        studentCourses.push(studentCourse)
+      }
     }
+  }
 
-    for (let i = 0; i < foundStudents.length; i ++){
-        let studentDiv = document.createElement("div");
-        studentDiv.classList.add("studentDiv");
+  for (let i = 0; i < foundStudents.length; i++) {
+    let studentDiv = document.createElement('div')
+    studentDiv.classList.add('studentDiv')
 
-        div.appendChild(studentDiv);
-        studentDiv.innerText = foundStudents[i].firstName + " " + foundStudents[i].lastName + " (" + studentCourses[i].passedCredits +" credits)" + "\n" 
-        + "Started: "+ studentCourses[i].started.semester + " " + studentCourses[i].started.year; 
-        
-        if (studentCourses[i].passedCredits == course.totalCredits){
-            studentDiv.style.backgroundColor = "blue";
-        }
+    div.appendChild(studentDiv)
+    studentDiv.innerText =
+      foundStudents[i].firstName +
+      ' ' +
+      foundStudents[i].lastName +
+      ' (' +
+      studentCourses[i].passedCredits +
+      ' credits)' +
+      '\n' +
+      'Started: ' +
+      studentCourses[i].started.semester +
+      ' ' +
+      studentCourses[i].started.year
+
+    if (studentCourses[i].passedCredits == course.totalCredits) {
+      studentDiv.style.backgroundColor = 'blue'
     }
+  }
 
-    let repsonsibleDiv = document.createElement("div"); 
-    repsonsibleDiv.innerHTML = "Course responsible: ";
-    repsonsibleDiv.classList.add("responsibleDiv");
-    div.appendChild(repsonsibleDiv);
+  let repsonsibleDiv = document.createElement('div')
+  repsonsibleDiv.innerHTML = 'Course responsible: '
+  repsonsibleDiv.classList.add('responsibleDiv')
+  div.appendChild(repsonsibleDiv)
 
+  for (let i = 0; i < theResponsibleTeacher.length; i++) {
+    let respP = document.createElement('p')
+    respP.classList.add('respP')
+    repsonsibleDiv.appendChild(respP)
+    respP.innerText =
+      theResponsibleTeacher[i].firstName +
+      ' ' +
+      theResponsibleTeacher[i].lastName +
+      ' (' +
+      theResponsibleTeacher[i].post +
+      ')'
+  }
 
-    for (let i = 0; i < theResponsibleTeacher.length; i++) {
-        let respP = document.createElement("p");
-        respP.classList.add("respP");
-        repsonsibleDiv.appendChild(respP);
-        respP.innerText = theResponsibleTeacher[i].firstName + " " + theResponsibleTeacher[i].lastName + " (" + theResponsibleTeacher[i].post + ")";
-    }
+  let teacherDiv = document.createElement('div')
+  teacherDiv.classList.add('teacherDiv')
+  div.appendChild(teacherDiv)
+  teacherDiv.innerText = 'Teachers: '
 
-    let teacherDiv = document.createElement("div");
-    teacherDiv.classList.add("teacherDiv");
-    div.appendChild(teacherDiv);
-    teacherDiv.innerText = "Teachers: ";
-
-    for(let i = 0; i < allTeachers.length; i++){
-        let teacherP = document.createElement("p");
-        teacherDiv.appendChild(teacherP);
-        teacherP.innerText =  allTeachers[i].firstName + " " + allTeachers[i].lastName + " (" + allTeachers[i].post + ")";
-    }
+  for (let i = 0; i < allTeachers.length; i++) {
+    let teacherP = document.createElement('p')
+    teacherDiv.appendChild(teacherP)
+    teacherP.innerText =
+      allTeachers[i].firstName +
+      ' ' +
+      allTeachers[i].lastName +
+      ' (' +
+      allTeachers[i].post +
+      ')'
+  }
 }
 
 // Loopar igenom kurserna och adderar HTML
 
 function createHTML (courses) {
-    for (let course of courses){
-        renderCourse(course);
-    }
+  for (let course of courses) {
+    renderCourse(course)
+  }
 }
 
 // hitta rätt student via studentID
-function getStudentsById(course) {
-    let foundStudents = [];
+function getStudentsById (course) {
+  let foundStudents = []
 
-    for (let student of DATABASE.students) {
-        for (let studentCourse of student.courses) {
-            if (studentCourse.courseId == course.courseId) {
-                foundStudents.push(student);
-            }
-        }
+  for (let student of DATABASE.students) {
+    for (let studentCourse of student.courses) {
+      if (studentCourse.courseId == course.courseId) {
+        foundStudents.push(student)
+      }
     }
-    return foundStudents;
+  }
+  return foundStudents
 }
 
 // hitta rätt ansvariga lärare
 function getResponsibleTeacher (course) {
-    let responsibleTeacher = [];
+  let responsibleTeacher = []
 
-    for (let teacher of DATABASE.teachers) {
-         if (teacher.teacherId == course.courseResponsible) {
-            responsibleTeacher.push(teacher);
-        }  
+  for (let teacher of DATABASE.teachers) {
+    if (teacher.teacherId == course.courseResponsible) {
+      responsibleTeacher.push(teacher)
     }
-    return responsibleTeacher;
+  }
+  return responsibleTeacher
 }
 
-
 // hitta alla lärare involverade i kursen
-function getAllTeachers (course){
-    let allTeachers = [];
+function getAllTeachers (course) {
+  let allTeachers = []
 
-    for (let teacher of DATABASE.teachers){
-        for(let oneTeacher of course.teachers)
-        if (teacher.teacherId == oneTeacher){
-            allTeachers.push(teacher);
-        }
-    }
-    return allTeachers;
+  for (let teacher of DATABASE.teachers) {
+    for (let oneTeacher of course.teachers)
+      if (teacher.teacherId == oneTeacher) {
+        allTeachers.push(teacher)
+      }
+  }
+  return allTeachers
 }
 
 // dark-mode
-let themeSwitch = document.querySelector('input');
+let themeSwitch = document.querySelector('input')
 
 themeSwitch.addEventListener('change', () => {
-    let wrapper = document.getElementById("wrapper");
-    let results = document.getElementById("results");
-    let body = document.querySelector("body");
-    wrapper.classList.toggle('dark-theme');
-    results.classList.toggle("dark-theme");
-    body.classList.toggle("dark-theme");
-
-});
+  let wrapper = document.getElementById('wrapper')
+  let results = document.getElementById('results')
+  let body = document.querySelector('body')
+  wrapper.classList.toggle('dark-theme')
+  results.classList.toggle('dark-theme')
+  body.classList.toggle('dark-theme')
+})
--- students.js
+++ students.js
@@ -1,114 +1,159 @@
 'use strict'
 
-let inputStudent = document.getElementById('students-search');
+let inputStudent = document.getElementById('students-search')
+
+// för att sökfältet ska vara tomt vid reload
+window.onload = function () {
+  inputStudent.value = ''
+}
 
 // hitta rätt student
 function findStudent () {
-  let student = DATABASE.students
-    .filter((student) => student.lastName.toLowerCase().includes(inputStudent.value.toLowerCase()));
+  let student = DATABASE.students.filter(student =>
+    student.lastName.toLowerCase().includes(inputStudent.value.toLowerCase())
+  )
 
-    // För att få studenterna i bokstavsordning på förnamn
-    student.sort(function (a, b) {
-        if (a.lastName > b.lastName) {
-            return 1;
-        } 
-        if (a.lastName < b.lastName) {
-            return -1;
-        }
-        return 0; 
-    });
+  // För att få studenterna i bokstavsordning på förnamn
+  student.sort(function (a, b) {
+    if (a.lastName > b.lastName) {
+      return 1
+    }
+    if (a.lastName < b.lastName) {
+      return -1
+    }
+    return 0
+  })
 
-  return student;
+  return student
 }
 
 // eventlyssnare för när användaren söker i input-fältet
 inputStudent.addEventListener('keyup', function () {
-  let foundStudent = findStudent();
-  document.getElementById("results").innerHTML = "";
-  createHTML(foundStudent);
+  let foundStudent = findStudent()
+  document.getElementById('results').innerHTML = ''
+  createHTML(foundStudent)
 
-  if (inputStudent.value == ""){
-    document.getElementById("results").innerHTML = "";
+  if (inputStudent.value == '') {
+    document.getElementById('results').innerHTML = ''
   }
-});
+})
 
 // visa rätt student med rätt innehåll i resultat-diven
 function renderStudent (student) {
-    let results = document.getElementById("results");
-    let div = document.createElement("div");
-    let studentName = document.createElement("h2");
-    div.classList.add("studentContainer");
+  let results = document.getElementById('results')
+  let div = document.createElement('div')
+  let studentName = document.createElement('h2')
+  div.classList.add('studentContainer')
 
-    let studentCourse = getStudentCourses(student);
-    let totalCredits = studentCourse.reduce(function(a, b){return a + b}, 0);
+  let studentCourse = getStudentCourses(student)
+  let totalCredits = studentCourse.reduce(function (a, b) {
+    return a + b
+  }, 0)
 
-    studentName.innerText = student.firstName + " " + student.lastName + " (total credits: " + totalCredits + ")";
-    results.appendChild(studentName);
-    results.appendChild(div);
+  studentName.innerText =
+    student.firstName +
+    ' ' +
+    student.lastName +
+    ' (total credits: ' +
+    totalCredits +
+    ')'
+  results.appendChild(studentName)
+  results.appendChild(div)
 
-    let foundCourses = getCourseById(student);
+  let foundCourses = getCourseById(student)
 
+  for (let i = 0; i < foundCourses.length; i++) {
+    let courseDiv = document.createElement('div')
+    courseDiv.classList.add('courseDiv')
 
-    for (let i = 0; i < foundCourses.length; i ++){
-        let courseDiv = document.createElement("div");
-        courseDiv.classList.add("courseDiv");
+    div.appendChild(courseDiv)
+    courseDiv.innerText =
+      foundCourses[i].title +
+      '\n' +
+      student.courses[i].started.semester +
+      ' ' +
+      student.courses[i].started.year +
+      ' / ' +
+      student.courses[i].passedCredits +
+      ' of ' +
+      foundCourses[i].totalCredits +
+      ' credits'
 
-        div.appendChild(courseDiv);
-        courseDiv.innerText = foundCourses[i].title + "\n" + student.courses[i].started.semester + " " + student.courses[i].started.year + " / " 
-        + student.courses[i].passedCredits + " of " + foundCourses[i].totalCredits + " credits";
-        
-        if (foundCourses[i].totalCredits == student.courses[i].passedCredits){
-            courseDiv.style.backgroundColor = "blue";
-        }
+    if (foundCourses[i].totalCredits == student.courses[i].passedCredits) {
+      courseDiv.style.backgroundColor = 'blue'
     }
-    
+  }
 }
 
 // Loopar igenom studenterna och adderar HTML
 
 function createHTML (students) {
-    for (let student of students){
-        renderStudent(student);
-    }
+  for (let student of students) {
+    renderStudent(student)
+  }
 }
 
 // hitta rätt kurs baserat på dess id
-function getCourseById(student) {
-    let foundCourses = [];
+function getCourseById (student) {
+  let foundCourses = []
 
-    for (let i = 0; i < student.courses.length; i ++) {
-        foundCourses.push(DATABASE.courses.find(course => {
-            return course.courseId == student.courses[i].courseId; 
-        }))
-    }
-    return foundCourses;
+  for (let i = 0; i < student.courses.length; i++) {
+    foundCourses.push(
+      DATABASE.courses.find(course => {
+        return course.courseId == student.courses[i].courseId
+      })
+    )
+  }
+  return foundCourses
 }
 
 // hitta rätt avklarade hp till varje student
 function getStudentCourses (student) {
-    
-    let studentCourses = [];
+  let studentCourses = []
 
-    for (let studentCourse of student.courses) {
-        for (let dbCourse of DATABASE.courses) {
-            if (studentCourse.courseId == dbCourse.courseId) {
-                studentCourses.push(studentCourse.passedCredits);
-            }
-        }
+  for (let studentCourse of student.courses) {
+    for (let dbCourse of DATABASE.courses) {
+      if (studentCourse.courseId == dbCourse.courseId) {
+        studentCourses.push(studentCourse.passedCredits)
+      }
     }
-    return studentCourses;
+  }
+  return studentCourses
 }
 
-// dark-mode
 
-let themeSwitch = document.querySelector('input');
+// försök till att få till local storage för dark-mode
+
+// let mode = localStorage.getItem("mode");
+
+// const chooseDark = () => {
+//     document.body.classList.add("dark-theme");
+//     document.body.classList.remove("light-theme");
+
+//     localStorage.setItem("mode", "dark-theme");
+// }
+
+// const chooseLight = () => {
+//     document.body.classList.add("light-theme");
+//     document.body.classList.remove("dark-theme");
+
+//     localStorage.setItem("mode", "light-theme");
+
+// }
+
+// if (mode === "dark-theme"){
+//     chooseDark();
+// } else if (mode === "light-theme"){
+//     chooseLight();
+// }
+
+let themeSwitch = document.querySelector('input')
 
 themeSwitch.addEventListener('change', () => {
-    let wrapper = document.getElementById("wrapper");
-    let results = document.getElementById("results");
-    let body = document.querySelector("body");
-    wrapper.classList.toggle('dark-theme');
-    results.classList.toggle("dark-theme");
-    body.classList.toggle("dark-theme");
-
-});
+  let wrapper = document.getElementById('wrapper')
+  let results = document.getElementById('results')
+  let body = document.querySelector('body')
+  wrapper.classList.toggle('dark-theme')
+  results.classList.toggle('dark-theme')
+  body.classList.toggle('dark-theme')
+})
